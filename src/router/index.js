67bfbb48db715f367f19e1ad9c69cf84c9e81224
Ricardo Franco <ricardosf195@gmail.com>
2022-11-30 22:09:36 +0000

    proper route verification

--- src/router/index.js
+++ src/router/index.js
@@ -1,111 +1,101 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "@/views/dashboard/HomeView.vue";
-import { useUserStore } from "@/stores/user";
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/views/dashboard/HomeView.vue';
+import { useUserStore } from '@/stores/user';
+import RouteRedirector from '@/components/RouteRedirector.vue';
 
 const routes = [
-  {
-    // Document title tag
-    // We combine it with defaultDocumentTitle set in `src/main.js` on router.afterEach hook
-    meta: {
-      title: "Dashboard",
+    {
+        path: '/redirect/:redirectTo',
+        name: 'Redirect',
+        component: RouteRedirector,
+        props: (route) => ({ redirectTo: route.params.redirectTo }),
     },
-    path: "/dashboard",
-    name: "dashboard",
-    component: Home,
-  },
-  {
-    meta: {
-      title: "Profile",
+    {
+        meta: {
+            title: 'Home',
+        },
+        path: '/',
+        name: 'home',
+        component: Home,
     },
-    path: "/profile",
-    name: "profile",
-    component: () => import("@/views/dashboard/ProfileView.vue"),
-  },
-  {
-    meta: {
-      title: "Login",
+    {
+        meta: {
+            title: 'Dashboard',
+        },
+        path: '/dashboard',
+        name: 'dashboard',
+        component: Home,
     },
-    path: "/login",
-    name: "login",
-    component: () => import("@/views/auth/LoginView.vue"),
-  },
-  {
-    meta: {
-      title: "Register",
+
+    {
+        meta: {
+            title: 'Profile',
+        },
+        path: '/profile',
+        name: 'profile',
+        component: () => import('@/views/dashboard/ProfileView.vue'),
     },
-    path: "/register",
-    name: "register",
-    component: () => import("@/views/auth/RegisterView.vue"),
-  },
+    {
+        meta: {
+            title: 'Login',
+        },
+        path: '/login',
+        name: 'login',
+        component: () => import('@/views/auth/LoginView.vue'),
+    },
+    {
+        meta: {
+            title: 'Register',
+        },
+        path: '/register',
+        name: 'register',
+        component: () => import('@/views/auth/RegisterView.vue'),
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
-  scrollBehavior(to, from, savedPosition) {
-    return savedPosition || { top: 0 };
-  },
+    history: createWebHistory(),
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        return savedPosition || { top: 0 };
+    },
 });
 
 /* Default title tag */
-const defaultDocumentTitle = "FasTuga";
+const defaultDocumentTitle = 'FasTuga';
 
-let handlingFirstRoute = true
+let handlingFirstRoute = true;
 
-router.beforeEach((to, from, next) => {  
-  // if (handlingFirstRoute) {
-  //   handlingFirstRoute = false
-  //   next({name: 'Redirect', params: {redirectTo: to.fullPath}})
-  //   return
-  // } else if (to.name == 'Redirect') {
-  //   next()
-  //   return
-  // }
-
-  // when user not auth --------------------
-  if (to.name == 'login') {
-    next()
-    return
-  }
-  
-  // careful 
-  // https://pinia.vuejs.org/core-concepts/outside-component-usage.html#single-page-applications
-  const userStore = useUserStore()  
-  
-  if (!userStore.user) {
-    next({ name: 'login' })
-    return
-  }
-  
-  // ----------------------------------------
-
-
-  // when user auth --------------------
-  if (to.name == 'Reports') {
-    if (userStore.user.type != 'A') {
-      next({ name: 'home' })
-      return
+router.beforeEach((to, from, next) => {
+    if (handlingFirstRoute) {
+        handlingFirstRoute = false;
+        next({ name: 'Redirect', params: { redirectTo: to.fullPath } });
+        return;
+    } else if (to.name == 'Redirect') {
+        next();
+        return;
     }
-  }
-  if (to.name == 'User') {
-    if ((userStore.user.type == 'A') || (userStore.user.id == to.params.id)) {
-      next()
-      return
+
+    // Careful!!! Using Handling first route to solve this
+    // https://pinia.vuejs.org/core-concepts/outside-component-usage.html#single-page-applications
+    const userStore = useUserStore();
+
+    // when user auth --------------------
+
+    if (userStore.user) {
+        if (to.name == 'login' || to.name == 'register') {
+            userStore.user.type == 'A' ? next({ name: 'dashboard' }) : next({ name: 'home' });
+        }
     }
-    next({ name: 'home' })
-    return
-  }
 
-  // ----------------------------------------
+    // ----------------------------------------
 
-  next()
-})
+    next();
+});
 
 /* Set document title from route meta */
 router.afterEach((to) => {
-  document.title = to.meta?.title
-    ? `${to.meta.title} — ${defaultDocumentTitle}`
-    : defaultDocumentTitle;
+    document.title = to.meta?.title ? `${to.meta.title} — ${defaultDocumentTitle}` : defaultDocumentTitle;
 });
 
 export default router;
