374f03956e8311209a81236284d1b0d3899338c3
Ricardo Franco <ricardosf195@gmail.com>
2022-12-13 20:07:41 +0000

    employee type in profile

--- src/components/dashboard/UserCard.vue
+++ src/components/dashboard/UserCard.vue
@@ -1,39 +1,50 @@
 <script setup>
-import { computed, ref } from "vue";
+import { computed, ref, onMounted } from "vue";
 import { useUserStore } from "@/stores/user";
 import { mdiCheckDecagram } from "@mdi/js";
 import BaseLevel from "@/components/dashboard/BaseLevel.vue";
 import UserAvatarCurrentUser from "@/components/dashboard/UserAvatarCurrentUser.vue";
 import CardBox from "@/components/dashboard/CardBox.vue";
-import FormCheckRadio from "@/components/dashboard/FormCheckRadio.vue";
 import PillTag from "@/components/dashboard/PillTag.vue";
 
 const userStore = useUserStore();
 
 const userName = computed(() => userStore.user?.name ?? "Anonymous");
 
-const userSwitchVal = ref(false);
+const userType = ref("");
+
+onMounted(() => {
+  switch (userStore.user?.type) {
+    case "EC":
+      userType.value = "Employee Chef";
+      break;
+
+    case "EM":
+      userType.value = "Employee Manager";
+      break;
+
+    case "ED":
+      userType.value = "Employee Deliver";
+      break;
+
+    default:
+      break;
+  }
+});
 </script>
 
 <template>
   <CardBox>
     <BaseLevel type="justify-around lg:justify-center">
-      <UserAvatarCurrentUser class="lg:mx-12" />
+      <UserAvatarCurrentUser class="w-48" />
       <div class="space-y-3 text-center md:text-left lg:mx-12">
-        <div class="flex justify-center md:block">
-          <FormCheckRadio
-            v-model="userSwitchVal"
-            name="notifications-switch"
-            type="switch"
-            label="Notifications"
-            :input-value="true"
-          />
-        </div>
         <h1 class="text-2xl">
           Howdy, <b>{{ userName }}</b
           >!
         </h1>
-        <p>Last login <b>12 mins ago</b> from <b>127.0.0.1</b></p>
+
+        <p>{{ userType }}</p>
+
         <div class="flex justify-center md:block">
           <PillTag label="Verified" color="info" :icon="mdiCheckDecagram" />
         </div>
